9a36037c260421e12ec7b2ffdf9034722916aa8f
Alexander Kolbeko <sasha-kolbeko@mail.ru>
2021-11-11 23:58:06 +0500

    continue frontend hw

--- frontend/homework 28.10.2021/task2.js
+++ frontend/homework 28.10.2021/task2.js
@@ -28,7 +28,7 @@ function findInObj(wordbook, keys){
 		result.push(wordbook[val])
 	});
 	return	result
-}
+};
 
 function addIsoDate(arr){
 	let resArr = [];
@@ -43,3 +43,66 @@ function addIsoDate(arr){
 	return resArr
 };
 
+function task22(year, month, arr){
+
+	let result = [];
+	result['replenishment'] = 0;
+	result['withdrawal'] = 0;
+
+	arr = arr.filter(function(item){
+		return item['year'] === year && item['month'] === month;
+	});
+
+	arr = addIsoDate(arr);
+
+	let balance = arr.reduce(function(prevVal, operation){
+		if (operation['type'] === 'payment' || operation['type'] === 'withdrawal'){
+			result['withdrawal'] += operation['amount']
+		}else {
+			result['replenishment'] += operation['amount']
+		};
+		
+		result['date'] = operation['date'];
+	}, 0);
+	
+	result['withdrawalRate'] = result['withdrawal'] / result['replenishment'] 
+	result['monthBalance'] = result['replenishment'] - result['withdrawal']
+
+	if(result['withdrawalRate'] < 0.15){
+		result['rank'] = 'Золотой';
+	}else if (result['withdrawalRate'] < 0.3){
+		result['rank'] = 'Серебрянный';
+	}else{
+		result['rank'] = 'Бронзовый';
+	};
+
+	return result
+};
+
+function task23(arr){
+
+	let uniq_dates = [];
+
+	arr.reduce(function(prevVal, item){
+		k_date = item['year'] + '-' + item['month'];
+		
+		if(! uniq_dates.hasOwnProperty(k_date)){
+			uniq_dates.push({'year': item['year'], 'month': item['month']})
+		}
+	});
+
+	let result = [];
+
+	uniq_dates.map(function(item, i){
+		result.push(task22(item['yearh'], item['month'], arr))
+		
+		if (i > 0){
+			result[i]['totalBalance'] = result[i-1]['totalBalance'] + result[i]['monthBalance']
+		}else{
+			result[i]['totalBalance'] = result[i]['monthBalance']
+		};
+
+	});
+
+	return result
+};
