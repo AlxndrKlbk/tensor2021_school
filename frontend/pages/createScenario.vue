aa6cfd06b579e4641ff617bd55063194dd369ba7
AlexPr0gram <aleksejvasilev756@mail.ru>
2021-12-15 22:57:34 +0500

    база поднята

--- frontend/pages/createScenario.vue
+++ frontend/pages/createScenario.vue
@@ -7,21 +7,16 @@
 export default {
   layout: 'create',
   asyncData ({ params, store, $axios }) {
-    const scenario = {
-      nameScenario: params.nameScenario,
-      articleScenario: params.articleScenario,
-      listLocation: store.getters['createScenario/getListLocation']
-    }
-    return {
-      scenario
-    }
+    // const scenario = {
+    //   nameScenario: params.nameScenario,
+    //   articleScenario: params.articleScenario,
+    //   listLocation: store.getters['createScenario/getListLocation']
+    // }
+    const scenario = $axios.$get('http://127.0.0.1:8888/scenarios_list')
+    return scenario
   },
-  data () {
-    return {
-      scenario: null
-    }
-  }
 }
+
 </script>
 
 <style>
